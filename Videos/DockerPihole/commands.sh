cab21d475694d19a72081748a0d7da6d147ba1e0
RPiList <56664851+RPiList@users.noreply.github.com>
2023-08-17 01:15:56 +0200

    Update commands.sh

--- Videos/DockerPihole/commands.sh
+++ Videos/DockerPihole/commands.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 mkdir /volume1/docker/pihole && \
 mkdir /volume1/docker/pihole/etc-pihole && \
-chmod 777 -R /volume1/docker/pihole/etc-pihole && \
 mkdir /volume1/docker/pihole/etc-dnsmasq.d && \
-chmod 777 -R /volume1/docker/pihole/etc-dnsmasq.d && \
 cd /volume1/docker/pihole && \
 wget https://raw.githubusercontent.com/RPiList/SynVideos/main/Videos/DockerPihole/docker-compose.yml && \
-docker-compose up -d
-
+docker-compose up -d && \
+chmod +t /volume1/docker/pihole && \
+chmod 776 -R /volume1/docker/pihole
