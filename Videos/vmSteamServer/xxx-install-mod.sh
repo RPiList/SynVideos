897f47844e6b84a1d37fe8062d60769634fb2153
RPiList <56664851+RPiList@users.noreply.github.com>
2025-03-04 04:58:00 +0100

    Delete Videos/vmSteamServer/xxx-install-mod.sh

--- Videos/vmSteamServer/xxx-install-mod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-steamcmd +runscript home/sprecher/arma3/xxx-install-mod.txt
-cd /home/sprecher/arma3/steamapps/workshop/content/107410/3407970596
-find . -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
-cd /home/sprecher/arma3
-./xxx-soft-link.sh 3407970596
-./xxx-copy-keys.sh
-
